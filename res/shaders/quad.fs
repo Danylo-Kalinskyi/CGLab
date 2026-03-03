41e6f4c1aecb52e5fe5aa6ab8d602e972fb0cdd5
Lomsss <dk.loms@gmail.com>
2026-03-03 13:57:19 +0100

    feat: 2.4

--- res/shaders/quad.fs
+++ res/shaders/quad.fs
@@ -10,17 +10,17 @@ void main()
 	vec3 color = vec3(0.0);
     float aspect = u_res.x / u_res.y;
 
-    // TASK 1
-    if (u_task == 1) {
+    // TASK 2
+    if (u_task == 2) {
 
-        // 1.a - gradient
+        // 2.a - gradient
         if (u_subtask == 0) {
             vec3 colorStart = vec3(0.0, 0.0, 1.0); 
             vec3 colorEnd = vec3(1.0, 0.0, 0.0);
             color = mix(colorStart, colorEnd, v_uv.x);
         }
 
-        // 1.b - gradient circle 
+        // 2.b - gradient circle 
         else if (u_subtask == 1) {
             vec2 p = (v_uv - 0.5);
             p.x *= aspect;
@@ -30,7 +30,7 @@ void main()
             color = vec3(clamp(gradient, 0.0, 1.0));
         }
 
-        // 1.c - stripes
+        // 2.c - stripes
         else if (u_subtask == 2 ) {
             // Pattern for each type of stripes
             float local_x = mod(v_uv.x * 5.0, 1.0);
@@ -47,15 +47,15 @@ void main()
             color = vec3(red_mask, 0.0, blue_mask);
         }
 
-        // 1.d - radial gradient
+        // 2.d - radial gradient
         else if (u_subtask == 3) {
             // Set up grid
-            vec2 p = v_uv * 10;
+            vec2 p = v_uv * 10.0;
             p.x *= aspect; // adapt dimensions
             vec2 grid = floor(p); // We use floor to find which square we are in
             // Normalize coordinates 
             float color_x = grid.x / (10.0 * aspect);
-            float color_y = grid.y / 10;
+            float color_y = grid.y / 10.0;
             // Define corner colors
             vec3 topLeft = vec3(0.0, 1.0, 0.0);
             vec3 topRight = vec3(1.0, 1.0, 0.0);
@@ -69,9 +69,9 @@ void main()
 
         }
 
-        // 1.e - grid
+        // 2.e - grid
         else if (u_subtask == 4) {
-            vec2 p = v_uv * 10;
+            vec2 p = v_uv * 10.0;
             p.x *= aspect; // adapt dimensions
             vec2 grid = floor(p);
             float check = mod(grid.x + grid.y, 2.0);
@@ -79,10 +79,10 @@ void main()
             color = vec3(check);
         }
 
-        // 1.f - sinusoid
+        // 2.f - sinusoid
         else if (u_subtask == 5) {
             // Sine wave
-            float wave = sin(v_uv.x * 6) * 0.3 + 0.5;
+            float wave = sin(v_uv.x * 6.0) * 0.3 + 0.5;
             // Top gradient
             float grad_top = 1.0 - v_uv.y;
             vec3 colorTop = vec3(0.0, grad_top, 0.0);
@@ -96,23 +96,23 @@ void main()
         }
     }
 
-    // TASK 2
-    else if (u_task == 2) {
+    // TASK 3
+    else if (u_task == 3) {
         vec4 texture_color = texture2D( u_tex, v_uv);
         color = texture_color.rgb;
 
-        // 2.a - greyscale
+        // 3.a - greyscale
         if (u_subtask == 0) {
             float gray = dot(color, vec3(0.299, 0.587, 0.114));
             color = vec3(gray);
         }
 
-        // 2.b - color inversion
+        // 3.b - color inversion
         else if (u_subtask == 1) {
             color = 1.0 - color;
         }
 
-        // 2.c - yellow
+        // 3.c - yellow
         else if (u_subtask == 2) {
             float r = dot(color, vec3(0.45, 0.85, 0.20));
             float g = dot(color, vec3(0.40, 0.80, 0.15));
@@ -120,13 +120,13 @@ void main()
             color = vec3(r, g, b);
         }
 
-        // 2.d - black and white
+        // 3.d - black and white
         else if (u_subtask == 3) {
             float brightness = dot(color, vec3(0.333));
             color = vec3(step(0.5, brightness));
         }
 
-        // 2.e - vignette
+        // 3.e - vignette
         else if (u_subtask == 4) {
             // Distance 
             float dist = distance(v_uv, vec2(0.5));
@@ -134,7 +134,7 @@ void main()
             color *= smoothstep(0.6, 0.05, dist);
         }
         
-        // 2.f - blur (3x3 box blur)
+        // 3.f - blur (3x3 box blur)
         else if (u_subtask == 5) {
             vec2 texelSize = vec2(5) / u_res; 
             vec3 sum = vec3(0.0);
@@ -150,6 +150,39 @@ void main()
             sum += texture2D(u_tex, v_uv + vec2( 1.0, -1.0) * texelSize).rgb;
             
             color = sum / 9.0;
+        }
+    }
+    // TASK 4
+    else if (u_task == 4) {
+        vec2 uv = v_uv;
+
+        // 3.a animated rotation
+        if (u_subtask == 0) {
+            // move origin to center (0.5, 0.5)
+            vec2 p = uv - 0.5;
+            
+            // get rotation angle based on time
+            float angle = u_time;
+            float s = sin(angle);
+            float c = cos(angle);
+            
+            // apply rotation matrix
+            vec2 rotatedP;
+            rotatedP.x = p.x * c - p.y * s;
+            rotatedP.y = p.x * s + p.y * c;
+            
+            // move origin back and sample
+            uv = rotatedP + 0.5;
+            color = texture2D(u_tex, uv).rgb;
+        }
+
+        // 3.b pixelization
+        else if (u_subtask == 1) {
+            // size = min size + ((range from -1 to 1 with frequency 0.25) + 1 to make it from 0 to 2) * amplitude/2 (as our range is from 0 to 1)
+            float size = 10.0 + (sin(u_time * 0.25) + 1.0) * 50.0;
+            vec2 pixelatedUV = floor(v_uv * size) / size;
+            
+            color = texture2D(u_tex, pixelatedUV).rgb;
         }
     }
     gl_FragColor = vec4(color, 1.0);
